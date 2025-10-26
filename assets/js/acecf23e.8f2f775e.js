9038342a79dc0657a97fd901a79c2d89e8d8c6fd
ernado <ernado@users.noreply.github.com>
2025-10-26 04:52:58 +0000

    deploy: 039ecfe7104e642dfd8987a60ee89901f62d89cd

---
+++ assets/js/acecf23e.8f2f775e.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkweb=globalThis.webpackChunkweb||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/blog","blogTitle":"Blog","authorsListPath":"/blog/authors"}')}}]);
