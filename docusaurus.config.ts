76e22a73a29307f2ca3cc5338767147ce16d81f8
Aleksandr Razumov <ernado@go-faster.org>
2025-10-31 12:46:29 +0300

    feat: social card

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,8 +69,7 @@ const config: Config = {
   ],
 
   themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/social-card.png',
     colorMode: {
       respectPrefersColorScheme: true,
     },
